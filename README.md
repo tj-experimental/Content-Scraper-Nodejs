f42d552ba19d5e7bcc87c7104bfc8abaad1eeb24
Tonye Jack <jtonye@ymail.com>
2016-09-17 17:58:15 -0400

    Added local filed

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Content-Scraper-Nodejs
 This project is a Node.js command line application that goes to an ecommerce site to gets the latest prices and saves them to a spreadsheet (CSV format). This spreadsheet will be used by another application to populate a database.This applications uses npm modules.
+
+An npm Package for Node.js using a third party npm package to scrape content from the site. 
+
+This creates a csv file
+
+The column headers Title, Price, ImageURL, URL and Time. 
+
+The current date time of when the scrape happened e.g. 2016-01-29.csv. 
+
+
+## Basic Usage
+
+```javascript
+var Scraper = require("content_scraper");
+
+var shirts4mikeScraper = new Scraper();
+
+/**
+* When the "end" event is triggered an array of prices
+* metadata is returned
+**/
+shirts4mikeScraper.on("end", console.dir);
+
+/**
+* If a parsing, network or HTTP error occurs an
+* error object is passed in to the handler or callback
+**/
+shirts4mikeScraper.on("error", console.error);
+```
+
+### Example of Returned Data
+
+```javascript
+[
+   {
+
+   },
+   {
+
+   }
+]
+
+```
+
+`title`, `link` and `author` are `string`s and `publishData` is a JavaScript `Date` object.
---
+++ scraper.js
@@ -0,0 +1,80 @@
+//<editor-fold desc="Description">
+/*The scraper should generate a folder called data if it doesn’t exist.
+
+The information from the site should be stored in a CSV file with today’s day e.g. 2016-01-29.csv.
+
+Use a third party npm package to scrape content from the site. 
+
+You should be able to explain why you chose that package.
+
+The scraper should be able to visit the website http://shirts4mike.com and follow links to all t-shirts.
+
+The scraper should get the price, title, url and image url from the product page and save it in the CSV.
+
+Use a third party npm package to create an CSV file. 
+You should be able to explain why you chose that package.
+
+The column headers should be in in this order Title, Price, ImageURL, URL and Time. 
+
+Time should be the current date time of when the scrape happened. 
+
+If they aren’t in this order the can’t be entered into the database of the price comparison site.
+
+If the site is down, an error message describing the issue should appear in the console. 
+
+This is to be tested by disabling wifi on your device.
+
+If the data file for today already exists it should overwrite the file.
+Code should be well documented.*/
+//</editor-fold>
+
+
+const http = require('http');
+var fs = require('fs');
+const https = require('https');
+const EventEmitter = require("events").EventEmitter;
+const util = require("util");
+
+
+
+function Scraper(){
+    
+    EventEmitter.call(this);
+
+    var scraperEmitter = this;
+
+    var request = http.get("http://shirts4mike.com", function(response) {
+        var body = "";
+
+        if (response.statusCode !== 200) {
+            request.abort();
+            //Status Code Error
+            scraperEmitter.emit("error", new Error("There was an error getting Prices from shirts4mike. (" + http.STATUS_CODES[response.statusCode] + ")"));
+        }
+
+        //Read the data
+        response.on('data', function (chunk) {
+            body += chunk;
+            scrapperEmitter.emit("data", chunk);
+        });
+
+        response.on('end', function(){
+        	if(response.statusCode === 200){
+        		try{
+
+        		}catch(error){
+
+        		}
+
+        	}
+        });
+    });
+
+}
+
+
+//Use a linting tool like ESLint to check your code for syntax errors and 
+//to ensure general code quality. You should be able to run npm run lint to check your code.
+//When an error occurs log it to a file scraper-error.log .
+// It should append to the bottom of the file 
+//with a time stamp and error e.g. [Tue Feb 16 2016 10:02:12 GMT-0800 (PST)] <error message>
